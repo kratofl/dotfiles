a324f79747e44a603f1cdfadca38ceee59f2ef8c
kratofl <l.achler@mopsoft.de>
2025-08-04 15:17:46 +0200

    Adjustments on gitsigns

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  keys = {
+    { "<leader>gp", mode = { "n" }, ":Gitsigns preview_hunk<CR>", desc = "Show hunk" },
+    { "<leader>gt", mode = { "n" }, ":Gitsigns toggle_current_line_blame<CR>", desc = "Toggle line blame" },
+  },
+  opts = {},
+}
